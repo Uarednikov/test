ba4cd892561cfbaec648c58b1ebed5e21ca2c1e7
Uarednikov <74322488+Uarednikov@users.noreply.github.com>
2023-04-21 11:48:06 +0300

    Update Обучение с учителем.py
    изменены гипер параметры модели .  max_depth=20, n_estimators=80,

--- Обучение с учителем.py
+++ Обучение с учителем.py
@@ -480,8 +480,8 @@ print('Лучший результат downsampled - ',
 # In[32]:
 
 
-model = RandomForestClassifier(bootstrap=True, class_weight='balanced', max_depth=8,
-                               n_estimators=40, random_state=12345)
+model = RandomForestClassifier(bootstrap=True, class_weight='balanced', max_depth=20,
+                               n_estimators=80, random_state=12345)
 
 model.fit(features_upsampled, target_upsampled)
 predicted_test = model.predict(features_test)
